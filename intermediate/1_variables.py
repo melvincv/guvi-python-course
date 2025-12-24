33ffe86b8f9b6d2385dedb9bf6408e3404f8fa32
Melvin C V <melrockz@gmail.com>
2025-12-24 02:18:05 +0000

    Variables

---
+++ intermediate/1_variables.py
@@ -0,0 +1,9 @@
+# Type Casting
+
+x=str(10)  # Convert integer to string
+y=int("20")  # Convert string to integer
+z=float("30.5")  # Convert string to float
+
+print(type(x))  # Output: <class 'str'>
+print(type(y))  # Output: <class 'int'>
+print(type(z))  # Output: <class 'float'>
